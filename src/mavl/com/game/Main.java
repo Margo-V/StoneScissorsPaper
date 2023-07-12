4ebd3b67738666aee62c6662e8ae009d8500e0da
Margarett <flautorfei1929@gmail.com>
2023-07-12 15:26:07 +0400

    Initial commit

---
+++ src/mavl/com/game/Main.java
@@ -0,0 +1,9 @@
+package mavl.com.game;
+
+public class Main {
+    public static void main(String[] args) {
+        StoneScissorsPaper game = new StoneScissorsPaper();
+        game.start();
+
+    }
+}
---
+++ src/mavl/com/game/StoneScissorsPaper.java
@@ -0,0 +1,111 @@
+package mavl.com.game;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StoneScissorsPaper {
+    ArrayList<String> arr=
+            new ArrayList<String>(List.of("Stone", "Scissors", "Paper"));
+
+    String user;
+    String robot;
+
+    String getWinner(String user, String robot){
+        if (user.equals(robot)){
+            return "A draw!";
+        }
+        else if( (user.equals("Stone") && robot.equals("Scissors")) ||
+                (user.equals("Scissors") && robot.equals("Paper")) ||
+                (user.equals("Paper") && robot.equals("Stone"))
+        ){
+            return "You won!";
+        }
+        else{
+            return "Robot";
+        }
+    }
+
+    String playerChoice(){
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.println((i + 1) + " " + arr.get(i));
+        }
+        String choice= sc.nextLine();
+
+        switch(choice){
+            case "1":
+                return arr.get(0);
+            case "2":
+                return arr.get(1);
+            case "3":
+                return arr.get(2);
+            default:
+                return "0";
+        }
+    }
+
+    String robotChoice(){
+        return arr.get((int)(Math.random()*(arr.size()-1)));
+    }
+    void play(){
+        System.out.println("Let's start! The first player will be:");
+        int playerQueue=(int) (Math.random()*2);
+
+        if (playerQueue==1) {
+            System.out.println("You first! Choose number of item: ");
+            user = playerChoice();
+            if (user.equals("0")) {
+                System.out.println("nothing found :(");
+                start();
+            } else {
+                System.out.println("Your answer: " + user);
+                System.out.println("\nRobot's making choose...");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException ex) {
+                }
+                robot = robotChoice();
+                System.out.println("Your answer: " + user + "\nRobot answer: " + robot);
+                System.out.println("WINNER: " + getWinner(user, robot));
+
+            }
+        }
+        else{
+            System.out.print("Robot first!");
+            try{
+                Thread.sleep(4000);
+            }catch (InterruptedException ex){}
+            robot=robotChoice();
+            System.out.println( "Robot have chosen item! \nLet's you! Choose number of item: ");
+            user = playerChoice();
+            if (user.equals("0")) {
+                System.out.println("nothing found :(");
+                start();
+            } else {
+                System.out.println("Your answer: " + user + "\nRobot's answer: " + robot);
+                System.out.println("WINNER: " + getWinner(user, robot));
+            }
+        }
+
+
+    }
+    void start(){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Hello!");
+        while (true) {
+            System.out.println("\nWould you like to play game with me? (yes/no):");
+            String answer = sc.nextLine().toLowerCase();
+            System.out.println();
+            if (answer.equals("yes")) {
+                play();
+            } else {
+                System.out.println("Oh ok, bye;)");
+                break;
+            }
+        }
+
+
+
+    }
+}
